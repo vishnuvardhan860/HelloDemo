be0aba8ae47ece95c918b948cacb00a174134b25
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-07-10 09:20:59 +0530

    Added:UC1

--- Hello.java
+++ Hello.java
@@ -2,7 +2,15 @@ class Hello
 {
         public static void main(String[] args)
         {
-                System.out.println("Hello");
-        }
+int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			 System.out.println("Employee is Absent");
+	                System.out.println("Hello");
+       
+
+ }
 }
 
